f9ab675b9fae76a853597daeb64df9c37ca30dfd
ekaterina <kat_klepko@mail.ru>
2023-03-11 13:14:18 +0300

    commit3

--- Program.cs
+++ Program.cs
@@ -52,6 +52,76 @@
 // Задайте прямоугольный двумерный массив. Напишите программу, 
 // которая будет находить строку с наименьшей суммой элементов.
 
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(0, 10);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j],3} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+// int[] Sum(int[,] array)
+// {
+//     int sum = 0;
+//     int[] array1 = new int[array.GetLength(0) + 1];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int temp = 0;
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             temp = temp + array[i, j];
+//             sum = temp;
+
+//         }
+//         Console.Write($"{sum} \t");
+//         array1[i] = sum;
+//     }
+//     return array1;
+// }
+
+// void PrintResult(int[] array)
+// {
+//     int min = array[0];
+//     int number = 0;
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         if (array[i] < min)
+//         {
+//             min = array[i];
+//             number = i + 1;
+//         }
+//     }
+//     Console.WriteLine($"Номер строки с наименьшей суммой элементов: {number}");
+// }
+
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, cols];
+// FillArray(array);
+// PrintArray(array);
+// Console.WriteLine();
+// PrintResult(Sum(array));
+
+
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
 void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -69,38 +139,15 @@ void PrintArray(int[,] array)
     }
 }
 
-int[] Sum(int[,] array)
+int[,] Proizv(int[,] array, int[,] array1)
 {
-    int sum = 0;
-    int[] array1 = new int[array.GetLength(0) + 1];
+    int[,] arrayProizv = new int[array.GetLength(0), array.GetLength(1)];
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        int temp = 0;
         for (int j = 0; j < array.GetLength(1); j++)
-        {
-            temp = temp + array[i, j];
-            sum = temp;
-
-        }
-        Console.Write($"{sum} \t");
-        array1[i] = sum;
+            arrayProizv[i, j] = array[i, j] * array1[i, j] + array[i, j + 1] * array1[i + 1, j];
     }
-    return array1;
-}
-
-void PrintResult(int[] array)
-{
-    int min = array[0];
-    int number = 0;
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        if (array[i] < min)
-        {
-            min = array[i];
-            number = i + 1;
-        }
-    }
-    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {number}");
+    return arrayProizv;
 }
 
 Console.WriteLine("Введите количество строк");
@@ -108,7 +155,10 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
 int cols = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[rows, cols];
+int[,] array1 = new int[rows, cols];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
-PrintResult(Sum(array));
+FillArray(array1);
+PrintArray(array1);
+Proizv(array, array1);
